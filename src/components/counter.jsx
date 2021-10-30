fb549bdd311039e274487906915291d36fb52f97
AmiraBasyouni <basyouniamira1@gmail.com>
2021-10-30 15:57:30 -0700

    1:21:26 countersjsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,23 +11,28 @@ class Counter extends Component {
 
   //   return <ul> {this.state.tags.map((tag) => <li key={tag}>{tag}</li>)} </ul>
   // }
-
   
-  constructor(){
-    super();
-    console.log('Constructor'. this)
-  }
+  // constructor(){
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-  handleIncrement(){
-    console.log('Increment Clicked', this);
+  handleIncrement = (product) => {
+    // console.log('Increment Clicked', this);
+    // this.state.count++;
+    console.log(product);
+    this.setState({ count: this.state.count + 1}); //explicitly
+  };
 
-  }
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1});
+  // };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={ (product) => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
         {/* {!this.state.tags.length && 'Please create a new tag!'}
         { this.renderTags()} */}
       </div>
