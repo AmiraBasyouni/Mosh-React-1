a4809472511262a4e3aab47b2e6ee26195686051
AmiraBasyouni <basyouniamira1@gmail.com>
2021-11-06 13:36:47 -0700

    link 5 change

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 0
     // tags: ['tag1', 'tag2', 'tag3'],
   };
 
@@ -17,10 +17,10 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = (product) => {
+  handleIncrement = () => {
     // console.log('Increment Clicked', this);
     // this.state.count++;
-    console.log(product);
+    // console.log(product);
     this.setState({ count: this.state.count + 1}); //explicitly
   };
 
@@ -29,15 +29,17 @@ class Counter extends Component {
   // };
 
   render() {
+    console.log('props', this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={ (product) => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={ this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         {/* {!this.state.tags.length && 'Please create a new tag!'}
         { this.renderTags()} */}
       </div>
     );
   }
+  
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
     classes += this.state.count === 0 ? 'warning' : 'primary';
